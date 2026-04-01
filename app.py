9af0c13c8f988fc2cc36871ce25d1da6d65c6a9a
wilson75 <tunkalwilson@gmail.com>
2026-04-01 10:48:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+
+a = 8
+b = 4
+
+def add(x, y):
+    return x + y
+
+result = add(a, b)
+
+print("Result is:", result)
---
+++ test_app.py
@@ -0,0 +1,6 @@
+# test_app.py
+
+from app import a, b, add
+
+def test_addition():
+    assert add(a, b) == 10
